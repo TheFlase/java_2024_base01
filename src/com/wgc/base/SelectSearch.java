2265a330d9bfac015f72ac3441df277628fb030b
theflash <luntan``12>
2024-04-21 17:11:06 +0800

    选择排序:使用泛型实现

--- src/com/wgc/base/SelectSearch.java
+++ src/com/wgc/base/SelectSearch.java
@@ -9,10 +9,10 @@ import java.util.Arrays;
  **/
 public class SelectSearch {
     public static void main(String[] args) {
-        int[] array = {2,4,1,5,7};
+        Integer[] array = {2,4,1,5,7};
         System.out.println("排序前："+ Arrays.toString(array));
-//        selectSearch(array);
-        selectTionSort(array);
+        selectSearch(array);
+//        selectTionSort(array);
         System.out.println("排序后："+ Arrays.toString(array));
     }
 
@@ -20,15 +20,15 @@ public class SelectSearch {
      * 选择排序
      * @param array
      */
-    public static void selectSearch(int[] array){
+    public static <E extends Comparable> void selectSearch(E[] array){
         int minIndex = 0;
-        int min = 0;
+        E min ;
         Boolean changeFlag = false;
         for(int i=0;i<array.length;i++){
             changeFlag = false;
             min = array[i];
             for(int j=i+1;j<array.length;j++){
-                if(min>array[j]){
+                if(min.compareTo(array[j])>0){
                     min = array[j];
                     minIndex = j;
                     changeFlag = true;
@@ -37,7 +37,7 @@ public class SelectSearch {
             if(changeFlag){
                 System.out.println("第"+(i+1)+"轮排序结束，最小的元素是："+array[minIndex]);
                 System.out.println("进行第"+(i+1)+"轮原始数据最小值交换");
-                int temp = array[i];
+                E temp = array[i];
                 array[i] = array[minIndex];
                 array[minIndex] = temp;
             }
@@ -48,11 +48,11 @@ public class SelectSearch {
      * 选择排序
      * @param array
      */
-    public static void selectTionSort(int[] array){
+    public static <E extends Comparable> void selectTionSort(E[] array){
         for(int i=0;i<array.length;i++){
             int minIndex = i;
             for(int j=i;j<array.length;j++){
-                if(array[j]<array[minIndex]){
+                if(array[j].compareTo(array[minIndex]) < 0){
                     minIndex = j;
                 }
             }
@@ -66,8 +66,8 @@ public class SelectSearch {
      * @param i
      * @param minIndex
      */
-    private static void swap(int[] array, int i, int minIndex) {
-        int temp = array[i];
+    private static <E> void swap(E[] array, int i, int minIndex) {
+        E temp = array[i];
         array[i] = array[minIndex];
         array[minIndex] = temp;
     }
